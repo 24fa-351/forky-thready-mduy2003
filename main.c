71c14d98098c94dabd8b16dadaaa209ccad63f23
mduy2003 <mduy2003@csu.fullerton.edu>
2024-10-06 10:32:15 -0700

    Added command line argument checks

--- main.c
+++ main.c
@@ -16,6 +16,12 @@ int main(int argc, char *argv[])
     sscanf(argv[2], "%d", &max);
     sscanf(argv[3], "%d", &pattern_number);
 
+    if (argc != 4)
+    {
+        printf("Usage: %s <min> <max> <pattern_number>\n", argv[0]);
+        return 1;
+    }
+
     int number_of_processes = max - min + 1;
     if (pattern_number == SIMULTANEOUS)
     {
@@ -24,6 +30,9 @@ int main(int argc, char *argv[])
     else if (pattern_number == SEQUENTIAL)
     {
         fork_pattern_two(number_of_processes);
+    } else {
+        printf("Invalid pattern number. Pick 1 or 2\n");
+        return 1;
     }
 
     return 0;
